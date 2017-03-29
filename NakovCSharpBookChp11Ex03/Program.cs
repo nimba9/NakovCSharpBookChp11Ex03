17cfc0aeac894cdc2d036b522352851aac446088
vancisin peter <vancisin.peter@gmail.com>
2017-03-29 22:50:43 +0200

    Add project files.

---
+++ NakovCSharpBookChp11Ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace NakovCSharpBookChp11Ex03
+{
+    class DayOfTheWeek
+    {
+        static void Main(string[] args)
+        {
+        
+           GetDayOfWeek();
+           
+        }
+
+        public static void GetDayOfWeek()
+        {
+            var now = DateTime.Now;
+            var day = System.DateTime.Now.DayOfWeek.ToString();
+            Console.WriteLine("Today is {0}, {1:d}", day, now);
+        }
+    }
+}
